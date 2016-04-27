75b5a817e232c96313f6054dea6e9d222898cc63
Yun-Tai Chang <yun-tai@bitmark.com>
2016-04-27 09:59:14 +0800

    remove redundant comments and printout

--- states.go
+++ states.go
@@ -10,7 +10,6 @@ import (
 	"encoding/binary"
 	"encoding/gob"
 	"errors"
-	"fmt"
 	zmq "github.com/pebbe/zmq4"
 	"reflect"
 	"time"
@@ -619,7 +618,7 @@ func (twoway *Bilateral) rpcClientRequestHandler(item interface{}) error {
 // allowing the Call to return any accumulated results
 func (twoway *Bilateral) finishRequest(request *rpcClientRequestData) {
 	log.Debugf("finish: %v", request)
-	fmt.Printf("finish: %v\n", request)
+
 	if nil != request.reply {
 		close(request.reply)
 	}
@@ -639,7 +638,6 @@ func (twoway *Bilateral) rpcClientResponseHandler(item interface{}) error {
 		timeout := item.(rpcClientTimeoutData)
 		if request, ok := twoway.rpcReturns[timeout.id]; ok {
 			log.Infof("timeout id: %d  request: %v", timeout.id, request)
-			fmt.Printf("timeout id: %d request: %v\n", timeout.id, request)
 			twoway.finishRequest(request)
 		}
 		return nil
